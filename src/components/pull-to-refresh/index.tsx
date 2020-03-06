9817e0bd23f2ea70e056d7501005e002c9574d6b
dangosky <1341514988@qq.com>
2020-03-06 23:15:04 +0800

    feat: init
    Change-Id: I2a66b23edcfc615a8ddc1dab6e7fc3d8e7ebe31f

---
+++ src/components/pull-to-refresh/index.tsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+class PullToRefresh extends PureComponent {
+  render() {
+    return (
+      <div>
+        <p>PullToRefresh</p>
+      </div>
+    )
+  }
+}
+
+export default PullToRefresh;
